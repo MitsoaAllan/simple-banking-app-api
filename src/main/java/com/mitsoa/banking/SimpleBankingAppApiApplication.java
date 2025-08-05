6b6c126e59ad5c5d024534509da593bf66b0f5dc
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-05 19:39:07 +0300

    feat: Initial commit

---
+++ src/main/java/com/mitsoa/banking/SimpleBankingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.mitsoa.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankingAppApiApplication.class, args);
+    }
+
+}
