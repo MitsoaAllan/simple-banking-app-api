5f4d6876f1e319f8b61048ae83761e59dae7426d
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-11 12:44:29 +0300

    feat(test) : 'POST /users '

---
+++ src/test/java/com/mitsoa/banking/endpoint/rest/controller/UserControllerTest.java
@@ -0,0 +1,65 @@
+package com.mitsoa.banking.endpoint.rest.controller;
+
+import com.mitsoa.banking.model.User;
+import com.mitsoa.banking.repository.UserRepository;
+import com.mitsoa.banking.repository.db.Datasource;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
+
+public class UserControllerTest {
+    private final UserRepository userRepositorySubject = new UserRepository(new Datasource());
+
+    @Test
+    void assert_true() {
+        assertTrue(true);
+    }
+    @Test
+    void save_all_repository() {
+        List<User> users = List.of(
+                new User(
+                        "RAJAONAH Mitantsoa Allan",
+                        "hei.mitsoa@gmail.com",
+                        LocalDate.of(2003,7,6)),
+                new User(
+                        "RAJAONAH Alisoa Megane",
+                        "hei.megane@gmail.com",
+                        LocalDate.of(2011,2,25))
+                );
+        List<User> actual = userRepositorySubject.saveAll(users);
+
+        assertTrue(actual.containsAll(users));
+        assertEquals(2, actual.size());
+        assertNotNull(actual);
+    }
+
+    @BeforeAll
+    static void setUp() throws SQLException {
+        var dataSource = new Datasource();
+        try(Connection conn = dataSource.getConnection();
+            Statement ps = conn.createStatement();
+        ){
+            ps.execute("delete from \"user\"");
+            ps.execute("alter sequence user_id_seq restart ");
+        }
+    }
+
+    @AfterAll
+    static void clear_data() throws SQLException {
+        var dataSource = new Datasource();
+        try(Connection conn = dataSource.getConnection();
+            Statement ps = conn.createStatement();
+        ){
+            ps.execute("delete from \"user\"");
+            ps.execute("alter sequence user_id_seq restart ");
+        }
+    }
+}
