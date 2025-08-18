2b703cbe7b6926f712692acfab334f9d7a348f4a
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-18 19:29:51 +0300

    feat(mapper): rest for mapper (not implemented yet)

---
+++ src/main/java/com/mitsoa/banking/endpoint/rest/mapper/UserRestMapper.java
@@ -0,0 +1,12 @@
+package com.mitsoa.banking.endpoint.rest.mapper;
+
+import com.mitsoa.banking.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRestMapper {
+
+    public com.mitsoa.banking.endpoint.rest.model.User apply(User user) {
+        return null;
+    }
+}
