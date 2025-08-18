4e34bca0a3208c24d7cd6e34e7fe300c7579ffe5
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-18 18:51:33 +0300

    feat(test): test updateByEmail method

--- src/test/java/com/mitsoa/banking/endpoint/rest/controller/UserControllerTest.java
+++ src/test/java/com/mitsoa/banking/endpoint/rest/controller/UserControllerTest.java
@@ -1,10 +1,13 @@
 package com.mitsoa.banking.endpoint.rest.controller;
 
+import com.mitsoa.banking.endpoint.rest.mapper.UserToUpdateMapper;
+import com.mitsoa.banking.endpoint.rest.model.UserToUpdate;
 import com.mitsoa.banking.exception.UserNotFoundException;
 import com.mitsoa.banking.model.User;
 import com.mitsoa.banking.repository.UserRepository;
 import com.mitsoa.banking.repository.db.Datasource;
 import com.mitsoa.banking.repository.mapper.UserMapper;
+import com.mitsoa.banking.service.UserService;
 import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +24,7 @@ import java.util.List;
 
 public class UserControllerTest {
     private final UserRepository userRepositorySubject = new UserRepository(new Datasource(),new UserMapper());
+    private final UserService userServiceSubject = new UserService(userRepositorySubject,new UserToUpdateMapper());
     List<User> users = List.of(
             new User(
                     1,
@@ -96,6 +100,32 @@ public class UserControllerTest {
         userRepositorySubject.deleteByEmail("hei.megane@gmail.com");
 
         assertEquals(userRepositorySubject.findAll(1,2),List.of());
+    }
+
+    @Test
+    void update_user_by_email_repository() throws UserNotFoundException {
+        userRepositorySubject.saveAll(users);
+        var userMitsoa = userRepositorySubject.findByEmail("hei.mitsoa@gmail.com");
+        var userMegane = userRepositorySubject.findByEmail("hei.megane@gmail.com");
+
+        userServiceSubject.updateByEmail(
+                userMitsoa.getEmail(),
+                new UserToUpdate(
+                        "RAJAONAH Mitsoa Allan",
+                        LocalDate.of(2003,7,7)
+                )
+        );
+        userServiceSubject.updateByEmail(
+                userMegane.getEmail(),
+                new UserToUpdate(
+                        "RAJAONAH Alisoa Gane",
+                        LocalDate.of(2011,2,24)
+                )
+        );
+        assertNotNull(userMitsoa);
+        assertNotNull(userMegane);
+        assertEquals("RAJAONAH Mitsoa Allan",userRepositorySubject.findByEmail(userMitsoa.getEmail()).getFullName());
+        assertEquals("RAJAONAH Alisoa Gane",userRepositorySubject.findByEmail(userMegane.getEmail()).getFullName());
     }
 
     @BeforeAll
